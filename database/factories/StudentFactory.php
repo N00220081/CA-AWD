22f6463f7a79522078f971bb278021cb31d49c9e
Christina <n00220081@iadt.ie>
2023-10-23 14:28:20 +0100

    first commit

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Student;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class BookFactory extends Factory
+{
+public function definition(): array
+{
+    return [
+        'name' => fake()->name,
+        'address' => fake()->address,
+        'dob' => fake()->date($format = 'd-m-y', $max = '2010',$min = '1900'),
+        'number' => fake()->phoneNumber,
+        // 'college_id' => college()->id
+            
+    
+        ];
+    }
+}
---
+++ database/migrations/2023_10_16_132752_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->date('dob');
+            $table->string('number');
+            $table->foreign('college_id')->reference('id')->on('college');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
