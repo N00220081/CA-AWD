950a3597626fdace7bc41bf27b9de6aa6024e43d
Christina <n00220081@iadt.ie>
2023-11-27 13:40:36 +0000

    Lesson 13

--- database/migrations/2023_11_27_113912_create_roles_table.php
+++ database/migrations/2023_11_27_113912_create_roles_table.php
@@ -4,16 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBooksTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //creates the students table when running the migration.
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ class CreateBooksTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('roles');
     }
 };
